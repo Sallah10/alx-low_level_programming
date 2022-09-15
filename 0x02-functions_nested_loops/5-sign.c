48e1d6c6a50473a3987b5d8d62d7212824c210a2
Muhammed Bello <mohsallah3310@gmail.com>
2022-09-14 20:15:39 -0700

    Task 6: done. Also, fixed a bug in task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 	int disp;
 
-	if (n < 0)
+	if (n > 0)
 	{
 		disp = 1;
 		_putchar('+');
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The given integer
+ *
+ * Return: The absolute value of integer n
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
