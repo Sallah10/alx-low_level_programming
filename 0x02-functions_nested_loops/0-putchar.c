29772a6e0328a46016371951c98b62afb507d8a3
Muhammed Bello <mohsallah3310@gmail.com>
2022-09-14 19:42:29 -0700

    Task 0: done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar(45);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+	return (0);
+}
