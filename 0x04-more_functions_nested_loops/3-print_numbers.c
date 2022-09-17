6d72eb6d4ecdf010b4565d8cfa3cd9615973795a
Muhammed Bello <mohsallah3310@gmail.com>
2022-09-16 18:38:49 -0700

    3: done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - lorem ipsum
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
