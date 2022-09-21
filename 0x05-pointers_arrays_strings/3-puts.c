1de064c335260c6e5fc502a33ecd23a107bb4b2a
Muhammed Bello <mohsallah3310@gmail.com>
2022-09-20 19:51:18 -0700

    3: done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts: lorem ipsum
+ * @str: lorem ipsum
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int index = 0;
+
+	while (*(str + index) != '\0')
+	{
+		putchar(*(str + index));
+		index++;
+	}
+	putchar(10);
+}
