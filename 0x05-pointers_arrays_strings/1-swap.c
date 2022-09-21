bc529e3cefd71d90a386b63596eee9777f323c1d
Muhammed Bello <mohsallah3310@gmail.com>
2022-09-20 19:43:23 -0700

    1: done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - lorem ipsum
+ * @a: first given input
+ * @b: second given input
+ *
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int init;
+
+	init = *a;
+	*a = *b;
+	*b = init;
+}
