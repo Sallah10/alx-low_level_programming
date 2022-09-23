29edc79282844b610fc6a4b4b750330df5d651bf
Muhammed Bello <mohsallah3310@gmail.com>
2022-09-22 20:43:50 -0700

    1: fix #1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,18 +2,18 @@
 
 /**
  * _strncat - concatenates two strings
- * @dest: destination string
- * @src: source string
- * @n: at most n bytes from src to dest
+ * @dest: lorem ipsum
+ * @src: lorem ipsum
+ * @n: lorem ipsum
  *
- * Return: a pointer to the resulting string dest
+ * Return: lorem ipsum
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
-
+	
 	int d_len;
-
+	
 	i = 0;
 	d_len = 0;
 	while (dest[i++])
