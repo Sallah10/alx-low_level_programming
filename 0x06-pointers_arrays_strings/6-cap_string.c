109ff4269031ff0b8e5cc476666448e96d4880d7
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2022-09-23 04:32:20 +0100

    Update 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - lorem ipsum
+ * @str: lorem ipsum
+ *
+ * Return: lorem ipsum
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			i++;
+		}
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
+		str[i - 1] == '\n' || str[i - 1] == ',' ||
+		str[i - 1] == ';' || str[i - 1] == '.' ||
+		str[i - 1] == '!' || str[i - 1] == '?' ||
+		str[i - 1] == '"' || str[i - 1] == '(' ||
+		str[i - 1] == ')' || str[i - 1] == '{' ||
+		str[i - 1] == '}' || i == 0)
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
